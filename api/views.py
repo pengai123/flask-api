c07430d5d6f7e9e3aebe97eb8f105c48627efc9c
Peng Ai <aaaipeng@gmail.com>
2021-05-24 22:36:31 -0700

    update views.py

--- api/views.py
+++ api/views.py
@@ -1,20 +1,26 @@
 from flask import Blueprint, jsonify, request
 from . import mongo
+import json
+from bson import json_util
 
 main = Blueprint("main", __name__)
 users = mongo.db.users
 
+
 @main.route("/")
 def homepage():
-	return "welcome to homepage"
+    return "welcome to homepage"
+
 
 @main.route("/add_user", methods=["POST"])
 def add_user():
-	new_user = request.get_json()
-	print("db users", users)
-	# user= users.insert_one(new_user)
-	return jsonify(new_user)
+    new_user = request.get_json()
+    print("db users", users)
+    user = users.insert_one(new_user)
+    print("user", json.dumps(user, default=json_util.default))
+    return jsonify("qweasd")
+
 
 @main.route("/users")
-def users():
-	return jsonify("qwe")
+def all_users():
+    return jsonify("qwe")
